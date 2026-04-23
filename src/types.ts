505db3ac439b64ae8b35e205efae1da6e558c8f8
John Twyman <john.twyman@greens.org.au>
2026-04-23 19:25:10 +1000

    feat: core types

---
+++ src/types.ts
@@ -0,0 +1,56 @@
+export type RuleSeverity = 'error' | 'warn' | 'off';
+
+export type BaseRuleOptions = {
+  except?: string[];
+};
+
+export type RequireErrorHierarchyOptions = BaseRuleOptions & {
+  rootErrorClass?: string;
+};
+
+export type MaxFileLinesOptions = BaseRuleOptions & {
+  tsx?: number;
+  ts?: number;
+};
+
+export type RequireTestTypeSuffixOptions = BaseRuleOptions & {
+  allowedSuffixes?: string[];
+};
+
+export type RuleConfig<O extends BaseRuleOptions = BaseRuleOptions> =
+  | RuleSeverity
+  | [RuleSeverity, O];
+
+export type RulesConfig = {
+  'no-apis-depend-on-components'?:          RuleConfig;
+  'no-apis-depend-on-pages'?:               RuleConfig;
+  'no-components-depend-on-pages'?:         RuleConfig;
+  'no-hooks-depend-on-pages'?:              RuleConfig;
+  'no-types-depend-on-runtime-layers'?:     RuleConfig;
+  'no-constants-depend-on-runtime-layers'?: RuleConfig;
+  'no-circular-dependencies'?:              RuleConfig;
+  'require-barrel-exports'?:                RuleConfig;
+  'require-path-alias'?:                    RuleConfig;
+  'require-error-hierarchy'?:               RuleConfig<RequireErrorHierarchyOptions>;
+  'errors-extend-ag-error'?:                RuleConfig;
+  'require-test-type-suffix'?:              RuleConfig<RequireTestTypeSuffixOptions>;
+  'require-hook-prefix'?:                   RuleConfig;
+  'max-file-lines'?:                        RuleConfig<MaxFileLinesOptions>;
+};
+
+export type ArchConfig = {
+  root: string;
+  tsConfigPath?: string;
+  mode: 'report' | 'enforce';
+  rules: RulesConfig;
+};
+
+export type Violation = {
+  file: string;
+  message: string;
+};
+
+export type RuleImplementation = (
+  config: ArchConfig,
+  options: BaseRuleOptions
+) => Promise<Violation[]>;
